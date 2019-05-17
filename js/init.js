7cd70512bec88231b19a0391e66ff21ccf1b868b
Kanji Hara Neto <kanjih@MACOAK0000038.local>
2019-05-17 15:23:33 -0700

    side-navbar

--- js/init.js
+++ js/init.js
@@ -19,37 +19,3 @@
 
 
 })(jQuery); // end of jQuery name space
-
-
-
-
-
-
-
-
-let restaurants,
-    neighborhoods,
-    cuisines
-var map
-var markers = []
-
-
-/**
- * Initialize Google map, called from HTML.
- */
-window.initMap = () => {
-  let loc = {
-    lat: 40.722216,
-    lng: -73.987501
-  };
-  self.map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 12,
-    center: loc,
-    scrollwheel: false,
-    mapTypeControl: false,
-    streetViewControl: false,
-    fullscreenControl:false,
-    zoomControl:false
-  });
-
-}
