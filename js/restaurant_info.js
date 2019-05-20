9b934ceace1e31eccc5f891d82e2b98c881a1cc2
Kanji Hara Neto <kanjih@MACOAK0000038.local>
2019-05-20 16:07:47 -0700

    finished responsive part

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -12,7 +12,12 @@ window.initMap = () => {
       self.map = new google.maps.Map(document.getElementById('map'), {
         zoom: 16,
         center: restaurant.latlng,
-        scrollwheel: false
+        scrollwheel: false,
+        mapTypeControl: false,
+        streetViewControl: false,
+        fullscreenControl:false,
+        zoomControl:false
+
       });
       fillBreadcrumb();
       DBHelper.mapMarkerForRestaurant(self.restaurant, self.map);
