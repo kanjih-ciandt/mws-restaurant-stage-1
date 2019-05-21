b045a3db28eba76ab06f15d2dde78c469c731b4a
Kanji Hara Neto <kanjih@MACOAK0000038.local>
2019-05-20 18:17:49 -0700

    concluded

---
+++ sw.js
@@ -0,0 +1,63 @@
+let CACHE_NAME = 'mws-restaurants-v1';
+let urlsToCache = [
+   '/',
+   '/css/materialize.css',
+   '/css/style.css',
+   '/data/restaurants.js',
+   '/img/1.jpg',
+   '/img/2.jpg',
+   '/img/3.jpg',
+   '/img/4.jpg',
+   '/img/5.jpg',
+   '/img/6.jpg',
+   '/img/7.jpg',
+   '/img/8.jpg',
+   '/img/9.jpg',
+   '/img/10.jpg',
+   '/js/dbhelper.js',
+   '/js/init.js',
+   '/js/main.js',
+   '/js/materialize.js',
+   '/js/restaurant_info.js'
+];
+
+self.addEventListener('install', function(event) {
+   // Perform install steps
+   event.waitUntil(
+       caches.open(CACHE_NAME)
+           .then(function(cache) {
+              console.log('Opened cache');
+              return cache.addAll(urlsToCache);
+           })
+   );
+});
+
+self.addEventListener('fetch', function(event) {
+   event.respondWith(
+       caches.match(event.request)
+           .then(function(response) {
+              // Cache hit - return response
+              if (response) {
+                 return response;
+              }
+
+              return fetch(event.request).then(
+                  function(response) {
+                     // Check if we received a valid response
+                     if(!response || response.status !== 200 || response.type !== 'basic') {
+                        return response;
+                     }
+
+                     let responseToCache = response.clone();
+
+                     caches.open(CACHE_NAME)
+                         .then(function(cache) {
+                            cache.put(event.request, responseToCache);
+                         });
+
+                     return response;
+                  }
+              );
+           })
+   );
+});
